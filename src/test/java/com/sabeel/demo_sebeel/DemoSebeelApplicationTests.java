dd6cde95e9a08507a170b7f5dc02f3e340d01816
habibamohamady32 <habibas032@gmail.com>
2025-06-17 21:17:18 +0300

    Initial commit

---
+++ src/test/java/com/sabeel/demo_sebeel/DemoSebeelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabeel.demo_sebeel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSebeelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
